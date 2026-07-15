bb9554b87c7ad0daa83d3578fff30a2143873a82
FelixHommel <felixhommel04@gmail.com>
2026-07-15 23:35:31 +0200

    refactor: remove unused

--- src/app/Application.hpp
+++ src/app/Application.hpp
@@ -1,11 +1,6 @@
 #ifndef FUL_SRC_APP_APPLICATION_HPP
 #define FUL_SRC_APP_APPLICATION_HPP
 
-#include "analysis/IAnalysis.hpp"
-#include "collection/ICollector.hpp"
-
-#include <memory>
-
 namespace ful::app
 {
 
@@ -19,10 +14,6 @@ public:
     Application& operator=(const Application&) = delete;
     Application(Application&&) = default;
     Application& operator=(Application&&) = default;
-
-private:
-    std::unique_ptr<ICollector> m_collector;
-    std::unique_ptr<IAnalysis> m_analyzer;
 };
 
 } // namespace ful::app
