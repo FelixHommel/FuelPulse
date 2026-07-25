958d4dbd90307743d675ba0554cd010f2637ce9c
FelixHommel <felixhommel04@gmail.com>
2026-07-25 18:19:51 +0200

    feat: add hash overloads for domain classes

--- src/lib/fuel/Domain.hpp
+++ src/lib/fuel/Domain.hpp
@@ -1,8 +1,12 @@
 #ifndef FUL_SRC_LIB_FUEL_DOMAIN_HPP
 #define FUL_SRC_LIB_FUEL_DOMAIN_HPP
 
+#include "utility/HashCombine.hpp"
+
 #include <chrono>
+#include <cstddef>
 #include <cstdint>
+#include <functional>
 #include <optional>
 #include <string>
 
@@ -39,5 +43,53 @@ struct Measurement
 };
 
 } // namespace ful::fuel
+
+namespace std
+{
+
+template<>
+struct hash<std::chrono::system_clock::time_point>
+{
+    constexpr std::size_t operator()(const std::chrono::system_clock::time_point& tp) const noexcept
+    {
+        return std::hash<std::chrono::system_clock::rep>{}(tp.time_since_epoch().count());
+    }
+};
+
+template<>
+struct hash<ful::fuel::Station>
+{
+    constexpr std::size_t operator()(const ful::fuel::Station& station) const noexcept
+    {
+        std::size_t seed{ 0 };
+
+        ful::hash::hashCombine(seed, station.id);
+        ful::hash::hashCombine(seed, station.name);
+        ful::hash::hashCombine(seed, station.brand);
+        ful::hash::hashCombine(seed, station.longitude);
+        ful::hash::hashCombine(seed, station.latitude);
+
+        return seed;
+    }
+};
+
+template<>
+struct hash<ful::fuel::Measurement>
+{
+    constexpr std::size_t operator()(const ful::fuel::Measurement& measurement) const noexcept
+    {
+        std::size_t seed{ 0 };
+
+        ful::hash::hashCombine(seed, measurement.stationId);
+        ful::hash::hashCombine(seed, measurement.timestamp);
+        ful::hash::hashCombine(seed, measurement.e5);
+        ful::hash::hashCombine(seed, measurement.e10);
+        ful::hash::hashCombine(seed, measurement.diesel);
+
+        return seed;
+    }
+};
+
+} // namespace std
 
 #endif // !FUL_SRC_LIB_FUEL_DOMAIN_HPP
---
+++ src/lib/utility/HashCombine.hpp
@@ -0,0 +1,29 @@
+#ifndef FUL_SRC_LIB_UTILITY_HASH_COMBINE_HPP
+#define FUL_SRC_LIB_UTILITY_HASH_COMBINE_HPP
+
+#include <cstddef>
+#include <functional>
+
+namespace ful::hash
+{
+
+// NOLINTBEGIN(readability-magic-numbers): For more info on the magic numbers see:
+//  https://stackoverflow.com/questions/4948780/magic-number-in-boosthash-combine
+
+/// \brief Boost hash_combine style hash function.
+///
+/// \tparam T The type that is hashed
+///
+/// \param seed The seed of the hash value
+/// \param value The object that is hashed
+template<typename T>
+constexpr void hashCombine(std::size_t& seed, const T& value)
+{
+    seed ^= std::hash<T>{}(value) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+}
+
+// NOLINTEND(readability-magic-numbers)
+
+} // namespace ful::hash
+
+#endif // !FUL_SRC_LIB_UTILITY_HASH_COMBINE_HPP
