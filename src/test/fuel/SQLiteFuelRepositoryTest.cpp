9ac89c46a0c3562f24fd929ed868e97911f650dd
FelixHommel <felixhommel04@gmail.com>
2026-07-26 03:27:50 +0200

    WIP: disable temporary test

--- src/test/fuel/SQLiteFuelRepositoryTest.cpp
+++ src/test/fuel/SQLiteFuelRepositoryTest.cpp
@@ -1,9 +1,9 @@
-#include "fuel/SQLiteFuelRepository.hpp"
-#include "fuel/Domain.hpp"
+// #include "fuel/SQLiteFuelRepository.hpp"
+// #include "fuel/Domain.hpp"
 
 #include <gtest/gtest.h>
 
-#include <chrono>
+// #include <chrono>
 #include <filesystem>
 
 namespace
@@ -16,18 +16,20 @@ inline const auto DB_PATH{ std::filesystem::path(TEST_RESOURCE_DIR) / "SQLiteFue
 namespace ful::testing
 {
 
-TEST(TEMP, TEMP)
-{
-    fuel::SQLiteFuelRepository repo{ ::DB_PATH };
+TEST(TEMP, TEMP) {}
 
-    const auto t{ std::chrono::system_clock::now() };
-    fuel::Measurement m{ .stationId = "123", .timestamp = t, .e5 = 123, .e10 = 123, .diesel = 123 };
-
-    repo.store(m);
-
-    auto x = repo.loadMeasurements(t - std::chrono::milliseconds(100), t + std::chrono::milliseconds(100));
-
-    ASSERT_EQ(x.size(), 1);
-}
+// TEST(TEMP, TEMP)
+// {
+//     fuel::SQLiteFuelRepository repo{ ::DB_PATH };
+//
+//     const auto t{ std::chrono::system_clock::now() };
+//     fuel::Measurement m{ .stationId = "123", .timestamp = t, .e5 = 123, .e10 = 123, .diesel = 123 };
+//
+//     repo.store(m);
+//
+//     auto x = repo.loadMeasurements(t - std::chrono::milliseconds(100), t + std::chrono::milliseconds(100));
+//
+//     ASSERT_EQ(x.size(), 1);
+// }
 
 } // namespace ful::testing
