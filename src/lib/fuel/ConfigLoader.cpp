3d35eb979f1f48274912825dc1d8fbd66a43320c
FelixHommel <felixhommel04@gmail.com>
2026-07-30 14:53:53 +0200

    feat: add configuration loader

---
+++ src/lib/fuel/ConfigLoader.cpp
@@ -0,0 +1,82 @@
+#include "ConfigLoader.hpp"
+
+#include "fuel/FuelPulseConfig.hpp"
+
+#include <nlohmann/json_fwd.hpp>
+#include <spdlog/spdlog.h>
+
+#include <cstdlib>
+#include <exception>
+#include <filesystem>
+#include <format>
+#include <fstream>
+#include <optional>
+#include <stdexcept>
+#include <string>
+
+namespace ful::fuel
+{
+
+namespace
+{
+
+/// \brief Attempt to load the Tanker Keonig API Key from the environment.
+///
+/// \returns The API Key in string form wrapped in a \ref std::optional if possible, \ref std::nullopt otherwise
+std::optional<std::string> loadApiKeyFromEnv()
+{
+    if(const char* pEnv = std::getenv("TANKER_KOENIG_API_KEY"))
+        return std::make_optional(pEnv);
+
+    spdlog::warn("Failed to find API Key in environment. Proceeding without being able to query.");
+    return std::nullopt;
+}
+
+/// \brief Load the JSON part of the \ref FuelPulseConfig.
+///
+/// \throws \ref std::runtime_error If the file does not exist or it couldn't be opened
+///
+/// \param configPath a \ref std::filesystem::path to the location of the configuration file
+FuelPulseConfig loadConfigFromFile(const std::filesystem::path& configPath)
+{
+    if(!std::filesystem::exists(configPath))
+        throw std::runtime_error(std::format("There is no file at the following location: {}", configPath.string()));
+
+    std::ifstream ifs{ configPath };
+
+    if(!ifs.is_open())
+        throw std::runtime_error(std::format("Unable to open file at the following location: {}", configPath.string()));
+
+    nlohmann::json data{ nlohmann::json::parse(ifs) };
+
+    return data.get<FuelPulseConfig>();
+}
+
+} // namespace
+
+FuelPulseConfig loadConfig(const std::filesystem::path& configPath)
+{
+    FuelPulseConfig config;
+    try
+    {
+        config = loadConfigFromFile(configPath);
+    }
+    catch(const std::exception& exc)
+    {
+        spdlog::warn(
+            "Failed to load the configuration from the following location: {}\n for the following reason: {}",
+            configPath.string(),
+            exc.what()
+        );
+        spdlog::info("Proceeding with default values in the config.");
+        config = {};
+
+        throw exc;
+    }
+
+    config.apiKey = loadApiKeyFromEnv();
+
+    return config;
+}
+
+} // namespace ful::fuel
---
+++ src/lib/fuel/ConfigLoader.hpp
@@ -0,0 +1,22 @@
+#ifndef FUL_SRC_LIB_FUEL_CONFIG_LOADER_HPP
+#define FUL_SRC_LIB_FUEL_CONFIG_LOADER_HPP
+
+#include "fuel/FuelPulseConfig.hpp"
+
+#include <filesystem>
+
+namespace ful::fuel
+{
+
+/// \brief Load a configuration file from the disk.
+///
+/// \param configPath The \ref std::filesystem::path to the location of the configuration file on the disk
+///
+/// \throws std::exception If loading the config from disk failed for some reason
+///
+/// \returns \ref FuelPulseConfig with the loaded values
+FuelPulseConfig loadConfig(const std::filesystem::path& configPath);
+
+} // namespace ful::fuel
+
+#endif // !FUL_SRC_LIB_FUEL_CONFIG_LOADER_HPP
