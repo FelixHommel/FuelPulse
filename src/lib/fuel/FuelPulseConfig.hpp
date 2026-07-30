ff21c8d1fea2472d75399bf91536b8f76546819c
FelixHommel <felixhommel04@gmail.com>
2026-07-30 14:49:08 +0200

    feat: add json deserialization function

--- src/lib/fuel/FuelPulseConfig.hpp
+++ src/lib/fuel/FuelPulseConfig.hpp
@@ -40,6 +40,19 @@ struct FuelPulseConfig
     std::filesystem::path reportDir{ DEFAULT_REPORT_DIR };
 };
 
+/// \brief nlohmann::json utility to allow for json::get<FuelPulseConfig>.
+void from_json(const nlohmann::json& j, FuelPulseConfig& config)
+{
+    j.at("max_stations").get_to<unsigned int>(config.maxStations);
+    j.at("postal_code").get_to<unsigned int>(config.postalCode);
+    j.at("search_radius").get_to<float>(config.searchRadius);
+    j.at("database_path").get_to<std::filesystem::path>(config.databasePath);
+    j.at("output_dir").get_to<std::filesystem::path>(config.reportDir);
+
+    const auto collectionInterval{ j.at("collection_interval").get<unsigned int>() };
+    config.collectionInterval = std::chrono::minutes(collectionInterval);
+}
+
 } // namespace ful::fuel
 
 #endif // !FUL_SRC_LIB_FUEL_FUEL_PULSE_CONFIG_HPP
