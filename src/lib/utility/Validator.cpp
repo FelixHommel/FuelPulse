21bb3dcdc5a87d91d94ac2c7803761b4ee9f2d5c
FelixHommel <felixhommel04@gmail.com>
2026-08-02 15:47:08 +0200

    refactor: split into header/source

---
+++ src/lib/utility/Validator.cpp
@@ -0,0 +1,38 @@
+#include "Validator.hpp"
+
+#include <nlohmann/json.hpp>
+#include <valijson/adapters/nlohmann_json_adapter.hpp>
+#include <valijson/schema.hpp>
+#include <valijson/schema_parser.hpp>
+#include <valijson/validation_results.hpp>
+#include <valijson/validator.hpp>
+
+#include <filesystem>
+#include <optional>
+#include <string>
+#include <utility>
+
+namespace ful
+{
+
+Validator::Validator(const std::string& jsonString)
+{
+    const auto schemaDoc = nlohmann::json::parse(jsonString);
+
+    valijson::SchemaParser parser;
+    valijson::adapters::NlohmannJsonAdapter schemaAdapter{ schemaDoc };
+    parser.populateSchema(schemaAdapter, schema);
+}
+
+Validator::Validator(const std::filesystem::path& schemaPath) : Validator{ readSchemaFile(schemaPath) } {}
+
+std::optional<valijson::ValidationResults> Validator::validate(const nlohmann::json& doc)
+{
+    valijson::adapters::NlohmannJsonAdapter docAdapter{ doc };
+    valijson::ValidationResults results;
+    const bool success{ valijson::Validator().validate(schema, docAdapter, &results) };
+
+    return (success ? std::nullopt : std::make_optional(std::move(results)));
+}
+
+} // namespace ful
--- src/lib/utility/Validator.hpp
+++ src/lib/utility/Validator.hpp
@@ -1,20 +1,15 @@
 #ifndef FUL_SRC_LIB_UTILITY_VALIDATOR_HPP
 #define FUL_SRC_LIB_UTILITY_VALIDATOR_HPP
 
-#include <nlohmann/json.hpp>
-#include <sstream>
-#include <string>
-#include <valijson/adapters/nlohmann_json_adapter.hpp>
+#include <nlohmann/json_fwd.hpp>
 #include <valijson/schema.hpp>
-#include <valijson/schema_parser.hpp>
 #include <valijson/validation_results.hpp>
-#include <valijson/validator.hpp>
 
 #include <filesystem>
 #include <fstream>
-#include <optional>
+#include <sstream>
 #include <stdexcept>
-#include <utility>
+#include <string>
 
 namespace ful
 {
@@ -45,15 +40,8 @@ std::string readSchemaFile(const std::filesystem::path& filePath)
 class Validator
 {
 public:
-    Validator(const std::string& jsonString)
-    {
-        const auto schemaDoc = nlohmann::json::parse(jsonString);
-
-        valijson::SchemaParser parser;
-        valijson::adapters::NlohmannJsonAdapter schemaAdapter{ schemaDoc };
-        parser.populateSchema(schemaAdapter, schema);
-    }
-    Validator(const std::filesystem::path& schemaPath) : Validator{ readSchemaFile(schemaPath) } {}
+    Validator(const std::string& jsonString);
+    Validator(const std::filesystem::path& schemaPath);
 
     /// \brief Validate the \p doc against the loaded schema.
     ///
@@ -61,14 +49,7 @@ public:
     ///
     /// \returns A \ref std::optional containing \ref valijson::ValidationResults if the validation failed, otherwise
     ///     contains a \ref std::nullopt
-    std::optional<valijson::ValidationResults> validate(const nlohmann::json& doc)
-    {
-        valijson::adapters::NlohmannJsonAdapter docAdapter{ doc };
-        valijson::ValidationResults results;
-        const bool success{ valijson::Validator().validate(schema, docAdapter, &results) };
-
-        return (success ? std::nullopt : std::make_optional(std::move(results)));
-    }
+    std::optional<valijson::ValidationResults> validate(const nlohmann::json& doc);
 
 private:
     valijson::Schema schema;
