4a797a374104561380b1d21ba7d5321e933ecfd1
FelixHommel <felixhommel04@gmail.com>
2026-08-04 00:29:34 +0200

    feat: add additional Validator tests

--- src/test/utility/validation/ValidatorTest.cpp
+++ src/test/utility/validation/ValidatorTest.cpp
@@ -3,11 +3,12 @@
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
-#include <memory>
 #include <nlohmann/json_fwd.hpp>
 
+#include <exception>
 #include <filesystem>
 #include <fstream>
+#include <memory>
 #include <string>
 
 namespace ful::testing
@@ -53,6 +54,21 @@ constexpr auto VALID_SCHEMA{ R"(
           "type": "string"
       }
   }
+}
+)" };
+
+constexpr auto INVALID_SCHEMA{ R"(
+{
+    "$schema": "https://json-schema.org/draft/2020-12/schema",
+    "title": "FuelPulseConfig",
+    "type": "object",
+    "properties": {
+        "max_stations": {
+            "description": "The maximum amount of stations to analyze"
+            "type": "integer"
+            "minimum": 1
+        }
+    }
 }
 )" };
 
@@ -122,11 +138,15 @@ TEST(ValidatorFromJsonStringTest, MultipleViolationsAreCaught)
     // NOLINTEND(bugprone-unchecked-optional-access)
 }
 
+/// \brief Test that a \ref Validator that is created from a filepath works as expected.
+///
+/// \author Felix Hommel
+/// \date 6/3/2026
 class ValidatorFromSchemaFileTest : public ::testing::Test
 {
 public:
     ValidatorFromSchemaFileTest() = default;
-    ~ValidatorFromSchemaFileTest() override { std::filesystem::remove(m_filePath); }
+    ~ValidatorFromSchemaFileTest() override = default;
 
     ValidatorFromSchemaFileTest(const ValidatorFromSchemaFileTest&) = delete;
     ValidatorFromSchemaFileTest& operator=(const ValidatorFromSchemaFileTest&) = delete;
@@ -139,6 +159,8 @@ public:
             std::filesystem::create_directory(TEST_RESOURCE_DIR);
     }
 
+    void TearDown() override { std::filesystem::remove(m_filePath); }
+
 protected:
     void prepareSchema(const std::string& schema)
     {
@@ -148,13 +170,13 @@ protected:
                    );
 
         std::ofstream ofs{ m_filePath };
-        const auto json = nlohmann::json::parse(schema);
-        ofs << json.dump(4);
+        ofs << schema;
     }
 
     std::filesystem::path m_filePath;
 };
 
+/// \brief Test that loading a valid JSON document as schema works without issues and validates a valid JSON document.
 TEST_F(ValidatorFromSchemaFileTest, ValidSchemaFileLoadsSchema)
 {
     prepareSchema(VALID_SCHEMA);
@@ -162,9 +184,17 @@ TEST_F(ValidatorFromSchemaFileTest, ValidSchemaFileLoadsSchema)
     std::unique_ptr<Validator> validator;
     EXPECT_NO_THROW(validator = std::make_unique<Validator>(m_filePath));
 
-    const auto result{ validator->validate(std::string(SCHEMA_CONFORM_JSON)) };
+    const auto result{ validator->validate(nlohmann::json::parse(std::string(SCHEMA_CONFORM_JSON))) };
 
     ASSERT_FALSE(result.has_value());
+}
+
+/// \brief Test that trying to create a \ref Validator with an invalid JSON document throws an exception.
+TEST_F(ValidatorFromSchemaFileTest, InvalidSchemaFileThrows)
+{
+    prepareSchema(INVALID_SCHEMA);
+
+    EXPECT_THROW(Validator validator{ m_filePath }, std::exception);
 }
 
 } // namespace ful::testing
