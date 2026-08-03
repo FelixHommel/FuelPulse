b0f1c9547ca493ce19bcda6a8692592d992250db
FelixHommel <felixhommel04@gmail.com>
2026-08-03 17:40:49 +0200

    feat: test Validator features

--- src/test/utility/validation/ValidatorTest.cpp
+++ src/test/utility/validation/ValidatorTest.cpp
@@ -1,10 +1,10 @@
 #include "utility/validation/Validator.hpp"
 
+#include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
-#include <nlohmann/json.hpp>
+#include <nlohmann/json_fwd.hpp>
 
-#include <iterator>
 #include <string>
 
 namespace ful::testing
@@ -12,8 +12,6 @@ namespace ful::testing
 
 namespace
 {
-
-using namespace nlohmann::literals;
 
 constexpr auto VALID_SCHEMA{ R"(
 {
@@ -29,7 +27,8 @@ constexpr auto VALID_SCHEMA{ R"(
       "postal_code": {
           "description": "The postal code around which to analyze the gas stations",
           "type": "integer",
-          "minimum": 1
+          "minimum": 1067,
+          "maximum": 99998
       },
       "search_radius": {
           "description": "The radius around which to search for gas stations",
@@ -43,7 +42,8 @@ constexpr auto VALID_SCHEMA{ R"(
       },
       "database_path": {
           "description": "Path to the database where the measurements and stations are stored",
-          "type": "string"
+          "type": "string",
+          "pattern": ".db3$"
       },
       "report_dir": {
           "description": "Path to the directory where the reports will be output to",
@@ -53,23 +53,69 @@ constexpr auto VALID_SCHEMA{ R"(
 }
 )" };
 
-const auto NON_CONFORM_JSON{ R"(
+const auto SCHEMA_CONFORM_JSON{ R"(
 {
-    "max_stations": -1
+    "max_stations": 1,
+    "postal_code": 1067,
+    "search_radius": 1.0,
+    "collection_interval": 1,
+    "databasePath": "db.db3",
+    "report_dir": "report/"
 }
-)"_json };
+)" };
 
 } // namespace
 
-TEST(ValidatorFromJsonStringTest, ValidatorFromValidJsonStringProducesValidator)
+/// \brief Test that passing a schema conform document to the validator reports no errors.
+TEST(ValidatorFromJsonStringTest, ValidDocumentReportsNoValidationErrors)
 {
     // NOLINTBEGIN(bugprone-unchecked-optional-access): Not unchecked access
-    Validator validator{ std::string(VALID_SCHEMA) };
+    const Validator validator{ std::string(VALID_SCHEMA) };
 
-    const auto r{ validator.validate(NON_CONFORM_JSON) };
+    const auto errors{ validator.validate(nlohmann::json::parse(std::string(SCHEMA_CONFORM_JSON))) };
 
-    ASSERT_TRUE(r.has_value());
-    EXPECT_EQ(std::distance((*r).begin(), (*r).end()), 1);
+    EXPECT_FALSE(errors.has_value());
+    // NOLINTEND(bugprone-unchecked-optional-access)
+}
+
+/// \brief Test that passing a document with an invalid 'max_stations' field reports back the error.
+TEST(ValidatorFromJsonStringTest, InvalidMaxStationsIsCaught)
+{
+    // NOLINTBEGIN(bugprone-unchecked-optional-access): Not unchecked access
+    const Validator validator{ std::string(VALID_SCHEMA) };
+
+    auto json = nlohmann::json::parse(std::string(SCHEMA_CONFORM_JSON));
+    json["postal_code"] = 01066; // NOLINT(readability-magic-numbers): 01067 is the smallest postal code in Germany
+
+    const auto result{ validator.validate(json) };
+    ASSERT_TRUE(result.has_value());
+
+    const auto& errors{ *result };
+    ASSERT_EQ(errors.size(), 1);
+    EXPECT_THAT(errors[0].path.to_string(), ::testing::HasSubstr("postal_code"));
+    EXPECT_THAT(errors[0].message, ::testing::HasSubstr("below minimum"));
+    // NOLINTEND(bugprone-unchecked-optional-access)
+}
+
+/// \brief Test that multiple schema validations are reported appropriately.
+TEST(ValidatorFromJsonStringTest, MultipleViolationsAreCaught)
+{
+    // NOLINTBEGIN(bugprone-unchecked-optional-access): Not unchecked access
+    const Validator validator{ std::string(VALID_SCHEMA) };
+
+    auto json = nlohmann::json::parse(std::string(SCHEMA_CONFORM_JSON));
+    json["postal_code"] = 99999; // NOLINT(readability-magic-numbers): 99998 is the largest postal code in Germany
+    json["database_path"] = "invalid.db";
+
+    const auto result{ validator.validate(json) };
+    ASSERT_TRUE(result.has_value());
+
+    const auto& errors{ *result };
+    ASSERT_EQ(errors.size(), 2);
+    EXPECT_THAT(errors[0].path.to_string(), ::testing::HasSubstr("database_path"));
+    EXPECT_THAT(errors[0].message, ::testing::HasSubstr("does not match regex pattern"));
+    EXPECT_THAT(errors[1].path.to_string(), ::testing::HasSubstr("postal_code"));
+    EXPECT_THAT(errors[1].message, ::testing::HasSubstr("exceeds maximum"));
     // NOLINTEND(bugprone-unchecked-optional-access)
 }
 
