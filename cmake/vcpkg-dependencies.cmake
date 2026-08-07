9580d49336820ddd7d72a12ff6a9571965f2b6d8
FelixHommel <felixhommel04@gmail.com>
2026-08-07 16:34:51 +0200

    refactor: fix order of dependencies

--- cmake/vcpkg-dependencies.cmake
+++ cmake/vcpkg-dependencies.cmake
@@ -1,8 +1,7 @@
 find_package(GTest CONFIG REQUIRED)
 
-find_package(nlohmann_json_schema_validator CONFIG REQUIRED)
-
 find_package(nlohmann_json CONFIG REQUIRED)
+find_package(nlohmann_json_schema_validator CONFIG REQUIRED)
 
 find_package(spdlog CONFIG REQUIRED)
 find_package(SQLiteCpp CONFIG REQUIRED)
